d69a6e790c49483c07a1f8c5f0ca480d71e2d229
small potato <38529172+fuheng1993@users.noreply.github.com>
2020-12-11 16:14:53 +0800

    Update dev.php
    测试提交

--- dev.php
+++ dev.php
@@ -3,7 +3,7 @@ return [
     'SERVER_NAME' => "easySiam",
     'MAIN_SERVER' => [
         'LISTEN_ADDRESS' => '0.0.0.0',
-        'PORT' => 9501,
+        'PORT' => 9525,
         'SERVER_TYPE' => EASYSWOOLE_WEB_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER,EASYSWOOLE_REDIS_SERVER
         'SOCK_TYPE' => SWOOLE_TCP,
         'RUN_MODEL' => SWOOLE_PROCESS,
@@ -28,9 +28,9 @@ return [
     'MYSQL' => [
         //数据库配置
         'host'                 => '127.0.0.1',//数据库连接ip
-        'user'                 => 'newadmin',//数据库用户名
-        'password'             => 'RdkfmTScjJPpHx4k',//数据库密码
-        'database'             => 'newadmin',//数据库
+        'user'                 => 'panel',//数据库用户名
+        'password'             => 'kGRZ8Ti2f4EGGAec',//数据库密码
+        'database'             => 'panel',//数据库
         'port'                 => '3306',//端口
         'timeout'              => '30',//超时时间
         'connect_timeout'      => '5',//连接超时时间
